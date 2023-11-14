2ce0d6b72a7d77551901108e73f73ffbf12bcce1
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-11-14 22:59:46 +0300

    Добавил highlight

--- src/components/header/HeaderAuth.js
+++ src/components/header/HeaderAuth.js
@@ -3,8 +3,8 @@ import React from 'react';
 function HeaderAuth() {
     return (
         <div className='header__auth flex'>
-            <h6>Регистрация</h6>
-            <h6 className='header__signin flex center text-black bg-green'>Войти</h6>
+            <h6 className='highlight'>Регистрация</h6>
+            <h6 className='header__signin flex center text-black bg-green highlight'>Войти</h6>
         </div>
     )
 }
--- src/components/promo/PromoMore.js
+++ src/components/promo/PromoMore.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function PromoMore() {
     return (
-        <button className='h6 font-medium promo__more'>Узнать больше</button>
+        <button className='h6 font-medium promo__more highlight'>Узнать больше</button>
     )
 }
 
