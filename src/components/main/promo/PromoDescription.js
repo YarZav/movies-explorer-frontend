c2046d0f64ca4f9c56518a4472d4d23203d24ad7
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-11-15 22:26:45 +0300

    Исправил Promo

--- src/components/main/promo/PromoDescription.js
+++ src/components/main/promo/PromoDescription.js
@@ -3,9 +3,9 @@ import Button from "../../common/button/Button";
 function PromoDescription() {
     return (
         <div>
-            <h1>Учебный проект студента<br />факультета<br />Веб-разработки.</h1>
+            <h1 className='promo__title'>Учебный проект студента факультета еб-разработки.</h1>
             <p className='promo__description'>Листайте ниже, чтобы узнать больше<br />про этот проект и его создателя.</p>
-            <Button textColor='text-white' text='Узнать больше' type='promo__button' />
+            <Button textColor='text-white' text='Узнать больше' type='promo' />
         </div>
     )
 }
