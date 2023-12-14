97379aa446f5c58027c5dbb16155d8bb7b8cc618
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-12-14 22:38:28 +0300

    Добавил ReadMe и исправил лого

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,20 @@
 import './Logo.css';
 
+import { useNavigate } from 'react-router-dom';
+
 import logo from '../../images/logo.svg';
 
 function Auth() {
+    const navigate = useNavigate();
+
+    function logoHandler() {
+        navigate('/');
+    }
+
     return(
-        <img className='logo header__logo_auth' src={logo} alt='Logo' />
+        <button className='logo__button highlight' onClick={logoHandler}>
+            <img className='logo header__logo_auth' src={logo} alt='Logo' />
+        </button>
     )
 }
 
